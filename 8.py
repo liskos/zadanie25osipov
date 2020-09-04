ab29ccb88a7ef5b9402d51ecd6e8dab907fc0289
kos <liskosnik@yandex.ru>
2020-09-04 18:38:25 +0300

    задание 8 версия 3 + вход,файл

--- 8.py
+++ 8.py
@@ -1,6 +1,11 @@
 # допускается также
 # использовать две
 # целочисленные переменные x и y
+import sys
+
+file = open(file="8.in", mode='r', encoding='utf8')
+sys.stdin = file
+
 b = []
 n = 30
 for i in range(0, n):
