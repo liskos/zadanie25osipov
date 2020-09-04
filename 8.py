13c10b6d5b43d857b8320a1f88c180ef00e8bfd8
kos <liskosnik@yandex.ru>
2020-09-04 18:29:33 +0300

    задание 8 версия 2

--- 8.py
+++ 8.py
@@ -6,7 +6,7 @@ n = 30
 for i in range(0, n):
     a.append(int(input()))
 b = []
-for i in range(0, n):
-    if a[i] >= 180:
-        b.append(a[i])
+for element in a:
+    if element >= 180:
+        b.append(element)
 print(min(b))
