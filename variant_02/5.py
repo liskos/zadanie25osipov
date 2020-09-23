b3111c4b45f99c239d6eeaf94d8e2ceea8e3541c
liskos <liskosnik2011@yandex.ru>
2020-09-23 17:20:08 +0300

    сделано 5 ver2

--- variant_02/5.py
+++ variant_02/5.py
@@ -1,37 +1,19 @@
-def d30(N):
-    x1 = str(N // 100)
-    x2 = str((N // 10) % 10)
-    x3 = str(N % 10)
-    if x1 >= x2 and x1 >= x3:
-        M = x1
-        if x2 > x3:
-            M += x2
-            m = x3 + x2
-        else:
-            M += x3
-            m = x2 + x3
-    elif x2 >= x1 and x2 >= x3:
-        M = x2
-        if x1 > x3:
-            M += x1
-            m = x3 + x1
-        else:
-            M += x3
-            m = x1 + x3
+def d30(n):
+    digits = list(map(int, str(n)))
+    digits.sort()
+    max_number = digits[2] * 10 + digits[1]
+    if digits[0] != 0:
+        min_number = digits[0] * 10 + digits[1]
+    elif digits[1] != 0:
+        min_number = digits[1] * 10
     else:
-        M = x3
-        if x2 > x1:
-            M += x2
-            m = x1 + x2
-        else:
-            M += x1
-            m = x2 + x1
-    R = int(M) - int(m)
-    return R
+        min_number = digits[2] * 10
+    return max_number - min_number
 
 
-k = 0
-for a in range(800,901):
-    if d30(a) == 30:
-        k += 1
-print(k)
+mas = []
+for i in range(800, 901):
+    if d30(i) == 30:
+        mas.append(i)
+print(mas)
+print(len(mas))
