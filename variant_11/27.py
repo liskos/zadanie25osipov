d6dafb4d23946382a8984137efed86fbc7e2c0d2
liskos <liskosnik2011@yandex.ru>
2021-02-06 16:56:17 +0300

    27+ вариант 11

--- variant_11/27.py
+++ variant_11/27.py
@@ -1,18 +1,19 @@
 import sys
-sys.stdin = open(file='Задание 27/27-A.txt', mode='r')
+sys.stdin = open(file='Задание 27/27-B.txt', mode='r')
+
 n = int(input())
-x = [0,0,0,0,0]
-s = 0
-for i in range(5):
+k = 6  # Требуемое расстояние между элементами
+otv = 0  # Ответ
+count = 0  # Количество чисел делящихся на 31, без последних k элементов
+mas = [int(input()) for _ in range(k)]  # Массив хранения последних k значений
+for i in range(k + 1, n + 1):
     a = int(input())
+    if mas[0] % 31 == 0:
+        count += 1
     if a % 31 == 0:
-        x[i % 5] = x[(i - 1) % 5]+1
-for i in range(5, n):
-    a = int(input())
-    if a % 31 == 0:
-        s += i - 5 #!!!
-        x[i%5] += 1
+        otv += i - k
     else:
-        s += x[i%5]
-        x[i % 5] = x[(i - 1) % 5]
-print(s)
+        otv += count
+    mas.pop(0)
+    mas.append(a)  # Удаляем первый элемент и вставляем значение переменной a на последнее место
+print(otv)
