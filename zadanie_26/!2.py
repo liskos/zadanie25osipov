1f8f9bdc50ae888605df5167e93e1f362b99fb5c
Kolya <kolyaosipovy2003@gmail.com>
2021-06-12 13:27:40 +0300

    zadanie 27 38 - 45

---
+++ zadanie_26/!2.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open(file='26data/26-2.txt')
+s, n = map(int, input().split())
+a = []
+for _ in range(n):
+    a.append(int(input()))
+a.sort()
+i = 0
+kol = 0
+for k in range(n):
+    if i + a[k] <= s:
+        i += a[k]
+        kol += 1
+    else:
+        break
+print(kol)
+print(s - i + a[kol - 1])
+print(a[kol - 1:])
